8d95ddc3e1044a0d528398da2d21a8f9373c3728
Edi Santoso <edicyber@gmail.com>
2015-04-17 14:49:48 +0700

    create new Uuid Model

---
+++ app/Entities/UuidModel.php
@@ -0,0 +1,87 @@
+<?php namespace SimdesApp\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
+
+class UuidModel extends Model
+{
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        /**
+         * Attach to the 'creating' Model Event to provide a UUID
+         * for the `id` field (provided by $model->getKeyName())
+         */
+        static::creating(function ($model) {
+            // generate _id
+            $model->{$model->getKeyName()} = (string)$model->generateNewId();
+
+            // created_at
+            $model->created_at = $model->dateNow();
+
+            //user creator
+            //$model->user_updater = \Auth::user()->_id;
+            //$model->organisasi_id = \Auth::user()->organisasi_id;
+            //$model->user_id = \Auth::user()->_id;
+        });
+
+        static::updating(function ($model) {
+            // updated_at
+            $model->updated_at = $model->dateNow();
+
+            // user updater
+            //$model->user_updater = \Auth::user()->_id;
+            //$model->organisasi_id = \Auth::user()->_id;
+        });
+    }
+
+    /**
+     * Generate new Uuid
+     *
+     * @return \Webpatser\Uuid\Uuid
+     * @throws \Exception
+     */
+    public function generateNewId()
+    {
+        return Uuid::generate(4);
+    }
+
+    /**
+     * Get Date now by Carbon
+     *
+     * @return static
+     */
+    public function dateNow()
+    {
+        return Carbon::now();
+    }
+
+    /**
+     * Get the user_id
+     *
+     * @return mixed
+     */
+    public function getUserId()
+    {
+
+    }
+
+    /**
+     * Get the organisasi_id
+     *
+     * @return mixed
+     */
+    public function getOrganisasiId()
+    {
+
+    }
+
+}
